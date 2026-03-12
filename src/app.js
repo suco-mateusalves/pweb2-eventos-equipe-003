9e8ab5d5d753760fa9a0cfada1e24c6d1476226b
Mateus Alves <153127373+suco-mateusalves@users.noreply.github.com>
2026-03-12 13:47:54 -0300

    fix: allow PATCH method in CORS configuration

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ app.use(helmet())
 
 app.use(cors({
    origin: ['http://localhost:3000'],
-   methods: ['GET', "POST", "PUT", "DELETE"],
+   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
    allowedHeaders: ["Content-Type", "Authorization"]
 }))
 
